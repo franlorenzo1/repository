f6df0809234ab2297500284c0ed285400cbcb130
franlorenzo1 <114301278+franlorenzo1@users.noreply.github.com>
2022-12-03 10:37:16 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+let ingresarEdad = parseInt(prompt("Ingresa tu edad"))
+if (ingresarEdad >= 18){
+    alert("Sos mayor de edad");
+}else{
+    alert("No sos mayor de edad");
+}
+
+for(var i = 1; i <= 10; i++){
+    alert(i)
+}
