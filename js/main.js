e91021a3e87e402d26012e5ab78eb9c53caac224
franlorenzo1 <114301278+franlorenzo1@users.noreply.github.com>
2022-12-24 18:42:43 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -9,3 +9,18 @@ if (ingresarEdad >= 18){
 for(var i = 1; i <= 10; i++){
     alert(i)
 }
+
+
+var nombresUsuarios;
+nombresUsuarios = prompt("Escribir su nombre de usuario")
+alert("su nombre de usuario es : " + nombresUsuarios)
+
+let resultado = 0;
+function sumar(primerNumero, segundoNumero){
+  resultado = primerNumero + segundoNumero
+}
+function mostrar(mensaje){
+    console.log(mensaje)
+}
+sumar(6, 3);
+mostrar(resultado);
